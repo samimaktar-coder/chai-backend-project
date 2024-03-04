fb38be7b18f8fee0de96fc3569eed0a9c567e400
Samim Aktar <samimaktar94320@gmail.com>
2024-03-04 19:46:27 +0530

    all models controllers and routes copied for assignment

--- src/app.js
+++ src/app.js
@@ -17,9 +17,17 @@ app.use(cookieParser());
 
 //route import
 import userRouter from './routes/user.routes.js';
+import commentRouter from './routes/comment.routes.js';
 
 //routes declaration
 app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 
