d619f3b6aaa06ca1bfb2331326e807d220b8d509
Samim Aktar <samimaktar94320@gmail.com>
2024-03-10 19:59:19 +0530

    minor update

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -6,58 +6,6 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-// const getChannelStats = asyncHandler(async (req, res) => {
-//     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-//     const channelStats = await Video.aggregate([
-//         {
-//             $match: {
-//                 owner: new mongoose.Types.ObjectId(req.user._id)
-//             }
-//         },
-//         {
-//             $lookup: {
-//                 from: 'subscriptions',
-//                 localField: 'owner',
-//                 foreignField: 'channel',
-//                 as: 'subscribers'
-//             }
-//         },
-
-//         {
-//             $lookup: {
-//                 from: 'likes',
-//                 localField: '_id',
-//                 foreignField: 'video',
-//                 as: 'likes'
-//             }
-//         },
-//         {
-//             $group: {
-//                 _id: 'owner',
-//                 views: { $push: '$views' }
-//             }
-//         },
-//         {
-//             $addFields: {
-//                 totalSubscribers: {
-//                     $size: "$subscribers"
-//                 },
-//                 totalLikes: {
-//                     $size: "$likes"
-//                 },
-//             }
-//         },
-//         {
-//             $project: {
-//                 totalSubscribers: 1,
-//                 totalLikes: 1
-//             }
-//         }
-//     ]);
-
-//     console.log(channelStats[0]);
-// });
-
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
